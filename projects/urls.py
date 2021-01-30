eb20700778026b5d01b0a828a1c3aad6b07c8341
fazer1929 <abhishekagrawal8888@gmail.com>
2021-01-30 16:48:55 +0530

    added examples for views

--- projects/urls.py
+++ projects/urls.py
@@ -3,4 +3,7 @@ from django.urls import path,include
 from projects import views
 urlpatterns = [
     path('',views.projectView),
+    path('addaprove/',views.addUserAproval),
+    path('addmembers/',views.addUserAproval),
+    path('addcomment/',views.addComments),
 ]
