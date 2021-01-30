89c57a6f39a5129bbd28ea08b493f69dfc69d7bb
Abhishek Agrawal <48363251+fazer1929@users.noreply.github.com>
2021-01-31 03:12:33 +0530

    Update settings

--- gocollab/settings.py
+++ gocollab/settings.py
@@ -123,6 +123,5 @@ STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS = [
     BASE_DIR / "static",
-    '/var/www/static/',
 ]
 django_heroku.settings(locals())
